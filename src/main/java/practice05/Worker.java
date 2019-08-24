8c974ba4b4af8c1e952b1c12dc394db20b1fb95c
watermelonsuperman <15538287230@163.com>
2019-08-24 20:08:47 +0800

    change introduce to Student and worker

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,12 @@
 package practice05;
 
-public class Worker {
+import practice04.Person;
+
+public class Worker extends Person{
+	public Worker(String name,int age){
+		super(name,age);
+	}
+	public String introduce(){
+		return super.introduce()+" I am a Worker. I have a job.";
+	}
 }
