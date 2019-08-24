f954559e62abecad699479a0f58e7edc965c923f
watermelonsuperman <15538287230@163.com>
2019-08-24 21:09:53 +0800

    add teacher

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+	private int klass;
+	public Teacher(String name,int age){
+		super(name,age);
+	}
+	public Teacher(String name,int age,int klass){
+		super(name,age);
+		this.klass = klass;
+	}
+	public int getKlass(){
+		return klass;
+	}
+	public String introduce(){
+		String result = "";
+		if(klass == 0){
+			result = super.introduce() + " I am a Teacher. I teach No Class.";
+		}else{
+			result = super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
+		}
+		return result;
+	}
 }
+
