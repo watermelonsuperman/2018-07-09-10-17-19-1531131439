7075d5688b491e81eb25d0033e0af5e7d0c97521
watermelonsuperman <15538287230@163.com>
2019-08-24 19:47:42 +0800

    change introduce to Student and worker

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,18 @@
 package practice04;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person{
+	private int klass;
+	public Student(String name, int age,int klass) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+		this.klass = klass;
+	}
+	public String introduce(){
+		return super.introduce()+" I am a Student. I am at Class "+ klass +".";
+	}
+	public int getKlass(){
+		return klass;
+	}
 }
